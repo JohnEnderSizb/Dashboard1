130a5e89976bab757fcb71b2c194835668335125
Johnson Andrew Siziba <siziba.internet@gmail.com>
2020-08-07 15:39:48 +0200

    wk

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Dashboard1
+
+![Alt text](./screenhot.png?raw=true "Title")
---
+++ app/src/main/java/com/agribank/echannels/dashboard1/MainActivity.java
@@ -0,0 +1,27 @@
+package com.agribank.echannels.dashboard1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        Dashboard dashboardFragment = new Dashboard();
+
+        fragmentTransaction.replace(R.id.container, dashboardFragment);
+        fragmentTransaction.commit();
+    }
+}
